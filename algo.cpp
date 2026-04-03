f0b2b97f6dee9e25e5c4d14ab81d9ac798e46089
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-03 14:42:30 +0700

    step 1,2,3

--- algo.cpp
+++ algo.cpp
@@ -33,4 +33,19 @@ void input()
     }
 }
 
+// create function mergeSort
+void mergeSort(int low, int high)
+{
+    if (low >= high) // step 1
+    {
+        return; // step 1.a
+    }
 
+    int mid = (low + high) / 2; // step 2
+
+    // step 3
+    // fungsi rekursif memanggil step sendiri
+    mergeSort(low, mid);       // step 3.a
+    mergeSort(mid + 1, high);  // step 3.b
+
+}
