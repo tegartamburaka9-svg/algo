ab51f2d43fb454e878495f2b5e222c91153c89c4
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-03 14:36:49 +0700

    memasukan input while(true)

--- algo.cpp
+++ algo.cpp
@@ -6,3 +6,21 @@ int arr[20], B[20];
 // n is array input size
 int n;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+
+}
