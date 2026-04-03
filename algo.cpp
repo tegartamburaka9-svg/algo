bc10933fd965a641605dd60aadbe56d2d6e1cf3f
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-03 14:39:30 +0700

    element array

--- algo.cpp
+++ algo.cpp
@@ -22,5 +22,15 @@ void input()
             cout << "\nMaksimal panjang array adalah 20";
         }
     }
+    cout << "\n------------------------------" << endl;
+    cout << "\nInputkan Isi element array" << endl;
+    cout << "\n------------------------------" << endl;
 
+    for (int i = 0; i < n; i++)
+    {
+        cout << "Array index ke - " << i << " : ";
+        cin >> arr[i];
+    }
 }
+
+
