242ac5000015c8fe42beee456e35a2ca3863d3c7
tegartamburaka9-svg <tegartamburaka9@gmail.com>
2026-04-03 14:49:48 +0700

    output

--- algo.cpp
+++ algo.cpp
@@ -89,3 +89,13 @@ void mergeSort(int low, int high)
     }
 }
 
+void output()
+{
+    cout << "\nData setelah diurutkan (Merge Sort): ";
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
